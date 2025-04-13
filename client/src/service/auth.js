62a9db0916bdd2bfe871eb7108d3e0d19221fc43
Gaurav1801 <gaurav.rt18@gmail.com>
2025-04-13 14:13:40 +0530

    change sinthe cilent

--- client/src/service/auth.js
+++ client/src/service/auth.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users";
+const API_URL = `${process.env.REACT_APP_API_URL}/api/users`;
 
-const registerUser = async (userData) => {
-  try {
-    const response = await axios.post(`${API_URL}/register`, userData);
-    return response.data;
-  } catch (error) {
-    console.error("Error registering user:", error);
-    throw error;
-  }
+const registerUser = async(userData) => {
+    try {
+        const response = await axios.post(`${API_URL}/register`, userData);
+        return response.data;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
 };
 
-const loginUser = async (userData) => {
-  try {
-    console.log(userData);
-    const response = await axios.post(`${API_URL}/login`, userData);
-    return response.data;
-  } catch (error) {
-    console.error("Error logging in user:", error);
-    throw error;
-  }
+const loginUser = async(userData) => {
+    try {
+        console.log(userData);
+        const response = await axios.post(`${API_URL}/login`, userData);
+        return response.data;
+    } catch (error) {
+        console.error("Error logging in user:", error);
+        throw error;
+    }
 };
 
 export { registerUser, loginUser };
--- client/src/service/users.js
+++ client/src/service/users.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/users";
+// const API_URL = "http://localhost:5000/api/users";
+
+const API_URL = `${process.env.REACT_APP_API_URL}/api/users`;
 
 
 
-
-const getAllUsers = async () => {
-  try {
-    const response = await axios.get(`${API_URL}/get-users`, );
-    return response.data;
-  } catch (error) {
-    console.error("Error registering user:", error);
-    throw error;
-  }
+const getAllUsers = async() => {
+    try {
+        const response = await axios.get(`${API_URL}/get-users`, );
+        return response.data;
+    } catch (error) {
+        console.error("Error registering user:", error);
+        throw error;
+    }
 };
 
 
 
-export {getAllUsers };
+export { getAllUsers };
